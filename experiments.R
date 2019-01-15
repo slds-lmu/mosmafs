203db228a2ec74f620ed3080a8621485d34256ce
mb706 <mlr.developer@mb706.com>
2019-01-15 16:02:34 +0100

    experimenting

--- experiments.R
+++ experiments.R
@@ -30,27 +30,30 @@ library("glmnet")
 library("parallel")
 library("BBmisc")
 
-cvmodel <- cv.glmnet(ldata$X, ldata$Y)
-
-
-besties <- order(-ldata$beta)
+## what is the sweet spot of selected number features?
+besties <- order(-ldata$beta)[1:10]
 perfs <- mclapply(seq_along(besties), function(selectfeats) {
   subdata <- ldata
   subdata$X <- subdata$X[, besties[seq_len(selectfeats)]]
   subtask <- create.classif.task("sublinear", subdata)
-  replicate(10, resample("classif.logreg", subtask, cv10))
+  replicate(1000, resample("classif.logreg", subtask, cv10), simplify = FALSE)
 }, mc.cores = 4)
+#save(perfs, ldata, file = "ldata_and_logreg_performances.Rsv")
 
-save(perfs, ldata, file = "ldata_and_logreg_performances.Rsv")
-
-lapply(perfs, function(perf) {
+plot(sapply(perfs, function(perf) {
   mean(extractSubList(perf, "aggr"))
-})
+}))
+lines(sapply(perfs, function(perf) {
+  perfs <- extractSubList(perf, "aggr")
+  mean(perfs) + sd(perfs)
+}))
+# compare:
+sum(ldata$orig.features)
 
 
 
-
-
+## What features does cvglmnet select?
+cvmodel <- cv.glmnet(ldata$X, ldata$Y)
 plot(cvmodel)
 
 names(ldata)
@@ -77,4 +80,10 @@ selorder[1:20]
 
 
 
+
+# --------- experimenting with ECR2
+
+# devtools::install_github("jakobbossek/ecr2")
+
+library("ecr")
 
