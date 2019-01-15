c33d80562928e5a6971ca9a332b63b66dd7a5b6f
mb706 <mlr.developer@mb706.com>
2019-01-15 10:18:32 +0100

    touching up data generation

--- datagen.R
+++ datagen.R
@@ -12,13 +12,13 @@ library("mlr")
 # ...
 # rho^p ...
 #
-# \epsilon normally distributed with stdev [error],
+# \epsilon normally distributed with stdev 1.
 # beta = shuffle(beta') and
 # and beta'[i] = beta0 * q ^ (i - 1) for i = 1..p
 #
 # Returns a list (X=[Matrix], Y=[vector], beta = [vector])
 #
-create.linear.data <- function(n, p, q = exp(-1), beta0 = 1, rho = 0, error = 1) {
+create.linear.data <- function(n, p, q = exp(-1), beta0 = 1, rho = 0) {
   cormat <- sapply(seq_len(p), function(x) rho ^ abs(x - seq_len(p)))
   X <- mvrnorm(n, rep(0, p), cormat)
   X <- X[, sample(p)]
@@ -26,26 +26,68 @@ create.linear.data <- function(n, p, q = exp(-1), beta0 = 1, rho = 0, error = 1)
   beta <- beta0 * q ^ seq(0, p - 1)
   beta <- sample(beta)
 
-  epsilon <- rnorm(n, sd = error)
+  epsilon <- rnorm(n)
 
   Y <- X %*% beta + epsilon
 
   list(X = X, Y = Y, beta = beta)
 }
 
-# returns list (task, beta)
-create.linear.regr.task <- function(id, ...) {
-  data <- create.linear.data(...)
-  list(task = makeRegrTask(id, data.frame(X = data$X, Y = data$Y), target = "Y"),
-    beta = data$beta)
+# Creates matrix X and vector Y:
+# sample [dim] columns of data from [dist] (which must be a function
+# `n -> vector length (n)` and should probably sample randomly to create X.
+# Y[i] is +1 if the L_`norm`-norm of X[i, ] is < 1, and -1 otherwise.
+# Returns list(X = [Matrix], Y = [vector]
+create.hypersphere.data <- function(dim, n, dist = function(x) runif(x, -1, 1), norm = 2) {
+  X = replicate(dim, dist(n))
+  Y = sign(1 - apply(X, 1, function(x) sum(x^norm))^(1/norm))
+  list(X = X, Y = Y)
 }
 
-# return list (task, beta, Y)
-# Task has a binary target depending on whether Y is less or greater than [cutoff]
-create.linear.classif.task <- function(id, cutoff = 0, ...) {
-  data <- create.linear.data(...)
-  list(task = makeClassifTask(id, data.frame(X = data$X, Y = ifelse(data$Y < cutoff, "-", "+")), target = "Y", positive = "+"),
-    beta = data$beta, Y = data$Y)
+# data must have $X and $Y
+create.regr.task <- function(id, data) {
+  makeRegrTask(id, data.frame(X = data$X, Y = data$Y), target = "Y")
+}
+
+# data must have $X and $Y
+# returns task with binary target Y => cutoff vs. Y < cutoff
+create.classif.task <- function(id, data, cutoff = 0) {
+  makeClassifTask(id, data.frame(X = data$X, Y = ifelse(data$Y < cutoff, "-", "+")), target = "Y", positive = "+")
 }
 
 
+# adds [num] new features sampled from [dist] to [task].
+# New features are inserted at random positions in the task
+# and named RANDOM.1 .. RANDOM.[num]
+#
+# The returned task has a new member `$orig.features` which
+# is a logical vector indicating the features that were originally
+# in the task.
+task.add.random.cols <- function(task, num, dist = rnorm) {
+  data <- task$env$data
+  target <- data[getTaskTargetNames(task)]
+  data[getTaskTargetNames(task)] <- NULL
+  orig.features <- sample(rep(c(TRUE, FALSE), c(ncol(data), num)))
+  newdata <- cbind(data, RANDOM = replicate(num, dist(nrow(data))))
+  stopifnot(length(orig.features) == ncol(newdata))
+  reorder <- vector("integer", length(orig.features))
+  reorder[orig.features] <- seq_len(ncol(data))
+  reorder[!orig.features] <- seq(ncol(data) + 1, ncol(newdata))
+  newdata <- newdata[reorder]
+  newdata <- cbind(newdata, target)
+  newid <- paste0(getTaskId(task), ".withrandom")
+
+  rettask = switch(getTaskType(task),
+    classif = {
+      makeClassifTask(newid, newdata,
+        getTaskTargetNames(task),
+        positive = getTaskDesc(task)$positive)
+    },
+    regr = {
+      makeRegrTask(newid, newdata,
+        getTaskTargetNames(task))
+    },
+    stop("Unknown task type."))
+  rettask$orig.features <- orig.features
+  rettask
+}
--- experiments.R
+++ experiments.R
@@ -4,12 +4,22 @@
 
 source("datagen.R")
 
+ldata <- create.linear.data(10000, 100)
+hdata <- create.hypersphere.data(2, 10000)
 
-task <- create.linear.classif.task("test", n = 1000, p = 100)$task
+ltask <- create.classif.task("linear", ldata)
+htask <- create.classif.task("linear", ldata)
 
-resample("classif.cvglmnet", task, cv10)
+htaskplus <- task.add.random.cols(htask, 5)
+str(getTaskData(htaskplus))
+
+resample("classif.cvglmnet", htask, cv10)
 
 resample("classif.logreg", task, cv10)
 
 resample("classif.randomForest", task, cv10)
+
+library("glmnet")
+
+plot(cv.glmnet(ldata$X, ldata$Y))
 
