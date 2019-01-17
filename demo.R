c41ecadee0f80c550f377146e7515c831e5d988a
mb706 <mlr.developer@mb706.com>
2019-01-17 23:01:04 +0100

    demo file

--- demo.R
+++ demo.R
@@ -7,10 +7,13 @@
 # * specify parameter set
 # * create operators
 # * create fitness function
-# *
+# * initialize population
+# * call ecr()
+# * analyse result
 
 
 # --- load libraries ---
+# devtools::install_github("jakobbossek/ecr2")
 source("datagen.R")
 source("ecrshims.R")
 source("selectorcpo.R")
@@ -21,8 +24,12 @@ library("ggplot2")
 library("parallelMap")
 
 # --- create task ---
-task <- create.hypersphere.data(4, 1000) %>%
-  create.classif.task(id = "hypersphere") %>%
+dataset <- create.hypersphere.data(4, 10000)
+keep <- apply(dataset$X, 1, function(x) all(x > 0))
+dataset$Y <- dataset$Y[keep]
+dataset$X <- dataset$X[keep, ]
+
+task <- dataset %>% create.classif.task(id = "hypersphere") %>%
   task.add.permuted.cols(5)
 
 # --- create learner ---
@@ -48,27 +55,30 @@ crossover <- combine.operators(ps,
   logical = recUnifCrossover)
 
 # --- create fitness function ---
+resinst <- makeResampleInstance(makeResampleDesc("CV", iters = 10, reps = 1, stratify = TRUE), task = task)
 fitness.fun <- function(args) {
-  val <- resample(setHyperPars(lrn, par.vals = args), task, cv10)$aggr
+  val <- resample(setHyperPars(lrn, par.vals = args), task, resinst, show.info = FALSE)$aggr
   propfeat <- mean(args$selector)
   c(val, propfeat)
 }
 
-
-MU <- 100
+# --- initialize population ---
+MU <- 15
+LAMBDA <- 4
 initials <- sampleValues(ps, MU, discrete.names = TRUE)
 
+# --- call ecr() ---
 parallelStartMulticore()
 
 results <- nsga2(fitness.fun = fitness.fun, n.objectives = 2, minimize = TRUE,
-  mu = MU, lambda = 4,
+  mu = MU, lambda = 32,
   mutator = mutator, recombinator = crossover,
   representation = "custom",
   initial.solutions = initials,
   log.pop = TRUE,
   terminators = list(stopOnIters(100)))
 
-
+# --- analyse result ---
 fitnesses <- function(results) {
   pops <- getPopulations(results$log)
   do.call(rbind, lapply(seq_along(pops), function(idx) {
@@ -80,16 +90,20 @@ fitnesses <- function(results) {
   }))
 }
 
+# pareto front
 plotFront(results$pareto.front)
 
+# development of pareto fronts
 ggplot(data = fitnesses(results), aes(x = perf, y = propfeat, color = iter)) + geom_point()
 
+# pareto set
 results$pareto.set
 
+# features in task that are original
+task$orig.features
+
+# population at generation x
+x <- 50
+getPopulations(results$log)[[x]]
 
 
-head(fitnesses(results))
-
-getPopulations(results$log)[[500]]
-
-getPopulations
