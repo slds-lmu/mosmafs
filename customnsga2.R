80aff06f3cffe142d4bcf302a3bb0814074eaacf
mb706 <mlr.developer@mb706.com>
2019-01-24 18:30:01 +0100

    use ecr::setup() instead of setup() b/c of nameclash

--- customnsga2.R
+++ customnsga2.R
@@ -3,8 +3,8 @@
 
 my.nsga2 = function(...) {
   args = list(
-    mutator = setup(mutPolynomial, eta = 25, p = 0.2),
-    recombinator = setup(recSBX, eta = 15, p = 0.7),
+    mutator = ecr::setup(mutPolynomial, eta = 25, p = 0.2),
+    recombinator = ecr::setup(recSBX, eta = 15, p = 0.7),
     representation = "float",
     parent.selector = selSimple,
     survival.selector = selNondom)
