60ae0238f16b32729cf6157227d82883a52bbc82
juliambr <ju.moosbauer@googlemail.com>
2019-01-26 16:19:23 +0100

    first version of benchmark

--- benchmark/benchmark.R
+++ benchmark/benchmark.R
@@ -1,6 +1,13 @@
 library(batchtools)
+library("magrittr")
 
 source("def.R")
+source("../datagen.R")
+source("../ecrshims.R")
+source("../selectorcpo.R")
+source("../customnsga2.R")
+source("../operators.R")
+
 
 if (file.exists("registry")) {
   if (OVERWRITE) {
@@ -16,22 +23,78 @@ if (file.exists("registry")) {
 }
 
 
-# generate the problems
-for (i in seq_along(TASK_IDS)) {
+for (i in seq_along(task.type)) {
 
-	id = names(TASK_IDS)[i]
-	task = getOMLTask(TASK_IDS[[i]])
+  id = names(task.type)[i]
+  data = task.type[i]
 
-	# do we have a dynamic part here? 
-    addProblem(reg = reg, name = id, data = task)
+  addProblem(reg = reg, name = id, data = data, fun = function(data, job, p.inf, p.noise, n) {
+    dataset = data[[1]](p.inf, n)
+    task = dataset %>% create.classif.task(id = names(data)) %>% task.add.random.cols(num = p.noise)
+     list(task = task)
+  })
 }
 
 
-featuresel.tune = function(data, job, instance, ...) {
+mosmafs = function(data, job, instance, learner, lambda, mu, maxeval) {
 
-	# need to specify what is going on here...
+  dataset = create.hypersphere.data(4, 100) 
+  task = dataset %>% create.classif.task(id = "test") %>% task.add.random.cols(num = 10)
+  learner = "SVM"
 
+  # task, learner and parameter set
+  task = instance$task
+  lrn = LEARNERS[[learner]]
+  resinst = makeResampleInstance(makeResampleDesc("CV", iters = 10, stratify = TRUE), task = task) 
+  
+  # --- parameter set ---
+  ps = PAR.SETS[[learner]]
+  ps = c(ps, pSS(selector.selection: logical^getTaskNFeats(task)))
+  
+  # --- create operators ---
+  mutator <- combine.operators(ps,
+    numeric = mutGauss,
+    logical = mutBitflip,
+    integer = mutUniformInt,
+    selector.selection = mutBitflip
+    )
+
+  crossover = combine.operators(ps,
+    numeric = recSBX,
+    integer = recIntSBX,
+    logical = recUnifCrossover)
+
+  # --- create fitness function ---
+  fitness.fun = function(args) {
+    args = args[intersect(names(args), getParamIds(getParamSet(lrn)))]
+    val = resample(setHyperPars(lrn, par.vals = args), task, resinst, show.info = FALSE)$aggr
+    propfeat = mean(args$selector)
+    c(val, propfeat)
+  }
+
+  initials = sampleValues(ps, mu, discrete.names = TRUE)
+
+  results = my.nsga2(
+    fitness.fun = fitness.fun, n.objectives = 2L, minimize = TRUE,
+    mu = mu, lambda = lambda,
+    mutator = mutator, recombinator = crossover,
+    representation = "custom",
+    initial.solutions = initials,
+    log.pop = TRUE,
+    terminators = list(stopOnEvals(maxeval)))
+
+  return(results)
 }
+
+addAlgorithm(name = "mosmafs", reg = reg, fun = mosmafs)
+
+addExperiments(reg = reg, 
+  prob.designs = pdes, 
+  algo.designs = list(mosmafs = ades),
+  repls = REPLICATIONS)
+
+# tab = summarizeExperiments(by = c("job.id", "problem", "p.inf", "p.noise", "n"))
+
 
 
 
--- benchmark/def.R
+++ benchmark/def.R
@@ -1,19 +1,52 @@
+library(data.table)
+
 # do not overwrite registry
 OVERWRITE = FALSE
 
+# --- Specify problem design ---
+# p.inf: number of informative features
+# p.noise: number of uninformative, noisy features
+# n: number of observations
+# task.type: hypersphere data or linear data
+p.inf = 4
+p.noise = c(10, 100, 1000)
+n = 1000
+
+task.type = list("hypersphere" = create.hypersphere.data)
+
+# problem design
+pdes = list("hypersphere" = CJ(p.inf = p.inf, p.noise = p.noise, n = n))
+
+# --- Specify algorithm design ---
+# This needs to be completed
+MU = 15L
+LAMBDA = 5L 
+MAXEVAL = 5L
+
+LEARNERS = list("SVM" = cpoSelector() %>>% makeLearner("classif.ksvm", kernel = "polydot"))
+
+PAR.SETS = list(
+	SVM = pSS(	  
+	degree: integer[1, 10],
+	C: numeric[0.1, 10]
+	)
+)
+
+ades = CJ(learner = c("SVM"), 
+	mu = MU, 
+	lambda = LAMBDA,
+	maxeval = MAXEVAL, 
+	sorted = FALSE)
+
+REPLICATIONS = 10
+
+
 # Datasets used by Bourani et al. 
-TASK_IDS = list("australian" = 146818, 
-				"ionosphere" = 116,
-				"heart" = 12717,
-				"pima" = 146241,
-				"glass" = 40,
-				"german" = 12715,
-				"sonar" = 269,
-				"vehicle" = 53)
-
-
-
-
-
-
-
+# TASK_IDS = list("australian" = 146818, 
+# 				"ionosphere" = 116,
+# 				"heart" = 12717,
+# 				"pima" = 146241,
+# 				"glass" = 40,
+# 				"german" = 12715,
+# 				"sonar" = 269,
+# 				"vehicle" = 53)
