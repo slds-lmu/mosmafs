3e01191f3d11b6b4963b6ec57b38f52b713f8a8b
juliambr <ju.moosbauer@googlemail.com>
2019-02-12 14:25:31 +0100

    create helper file

---
+++ benchmark/helpers.R
@@ -0,0 +1,21 @@
+# helpers
+
+fitnesses <- function(results) {
+  pops <- getPopulations(results$log)
+  do.call(rbind, lapply(seq_along(pops), function(idx) {
+    pop <- pops[[idx]]
+    df <- as.data.frame(t(pop$fitness))
+    colnames(df) <- c("perf", "propfeat")
+    df$iter <- idx
+    df
+  }))
+}
+
+
+getAllIndividuals = function(ecr_res) {
+	pops = lapply(getPopulations(ecr_res$log), function(x) do.call("rbind", lapply(x$population, unlist)))
+	pops = lapply(1:length(pops), function(i) cbind(i, pops[[i]]))
+	pops = do.call("rbind", pops)
+	pops = pops[!duplicated(pops[, -1]), ]
+
+}
