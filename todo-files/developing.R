0c2a3287dfc052c6d7f5e29fd06271f84a708ea2
mb706 <mlr.developer@mb706.com>
2019-02-28 15:10:47 +0100

    document new loggin features

--- todo-files/developing.R
+++ todo-files/developing.R
@@ -8,9 +8,42 @@ devtools::load_all("..")
 
 devtools::test("..")
 
+Sys.setenv("FASTVIGNETTE" = "true")
+
 tools::buildVignettes(dir = "..")
 
 
 
 
 parallelStop()
+
+x <- 1
+list(a = system.time(x <- x + 1)[3], b = x)
+
+parallelStop()
+data.table::setDTthreads(1)
+
+profvis::profvis(run.simple <- slickEcr(
+  fitness.fun = makeObjective(lrn, task, ps.simple, cv5),
+  lambda = 32,
+  population = initials.simple,
+  mutator = mutator.simple,
+  recombinator = crossover.simple,
+  parent.selector = selTournamentMO,
+  generations = 3))
+
+
+parallelStartMulticore(mc.set.seed = FALSE)
+
+run.simple <- slickEcr(
+  fitness.fun = makeObjective(lrn, task, ps.simple, cv5),
+  lambda = 32,
+  population = initials.simple,
+  mutator = mutator.simple,
+  recombinator = crossover.simple,
+  parent.selector = selTournamentMO,
+  generations = 3)
+
+getStatistics(run.simple$log)
+getStatistics(run.simple$log.newinds)
+
