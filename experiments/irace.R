deb2d53e2c341654910fa8b2623b8eaff9afc53b
mb706 <mlr.developer@mb706.com>
2019-03-05 22:50:29 +0100

    cleaning up experiment demo

--- experiments/irace.R
+++ experiments/irace.R
@@ -1,28 +1,40 @@
 
 
-
+library("mlr")
+library("ecr")
+library("mlrCPO")
+library("magrittr")
 devtools::load_all("..")
 
 spheretask <- create.hypersphere.data(3, 200, radius = 1) %>%
   create.classif.task(id = "sphere") %>%
   task.add.permuted.cols(5)
 
-lrn <- makeLearner("classif.ksvm", predict.type = "prob")
-
-lrn.ps <- pSS(
-  C: numeric[10^(-3), 10^3], # according to Fröhlich et al.
-  sigma: numeric[10^(-3), 10^3]
-)
+## lrn <- makeLearner("classif.ksvm", predict.type = "prob")
+##
+## lrn.ps <- pSS(
+##   C: numeric[10^(-3), 10^3], # according to Fröhlich et al.
+##   sigma: numeric[10^(-3), 10^3]
+## )
 
 lrn <- makeLearner("classif.rpart", maxsurrogate = 0)
+lrn.ps <- pSS(
+  maxdepth: integer[1, 30],
+  minsplit: integer[2, 30],
+  cp: numeric[0.001, 0.999])
 
 efun <- constructEvalSetting(
-    spheretask, lrn, lrn.ps, measure = mlr::auc, evals = 1e3)
+    spheretask, lrn, lrn.ps, measure = mlr::mmce, evals = 1e4,
+    savedir = ".")
 
 input <- sampleValue(getParamSet(efun), discrete.names = TRUE, trafo = TRUE)
+
+# rl <- readLines("/projects/user/mosmafstraces/run_22.out")
+# input <- eval(parse(text = rl[1]))
+
+
+# parallelMap::parallelStart
 input <- valuesFromNames(getParamSet(efun), input)
 
 returned <- efun(input)
 
-
-parallelMap::parallelStop()
